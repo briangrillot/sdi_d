065ed796c9d1ddc60913665454f91237ed091925
Pankace <briangrillot@hotmail.fr>
2020-05-20 11:59:20 +0200

    TP1

---
+++ HostApplication/HostApplication/HostObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostApplication
+{
+    class HostObject : MarshalByRefObject, RemotingObjects.IRemoteObject
+    {
+        public String sayHello (String name)
+        {
+            return "Hello" + name;
+        }
+    }
+}
---
+++ WindowsFormsClient/WindowsFormsClient/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsClient
+{
+    public partial class Form1 : Form
+    {
+        private RemotingObjects.IRemoteObject remoteObject;
+        public Form1()
+        {
+            try
+            {
+                TcpChannel channel = new TcpChannel();
+                ChannelServices.RegisterChannel(channel, true);
+                remoteObject = (RemotingObjects.IRemoteObject)
+                    Activator.GetObject(typeof(RemotingObjects.IRemoteObject), "tcp://localhost:12000/HostObject");
+            }
+            catch
+            {
+                MessageBox.Show("Error connecting to the server");
+            }
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (remoteObject != null)
+            {
+                String result = remoteObject.sayHello(textBox1.Text);
+                MessageBox.Show("Result : " + result);
+            }
+            else
+            {
+                MessageBox.Show("Error: remote object not available");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
